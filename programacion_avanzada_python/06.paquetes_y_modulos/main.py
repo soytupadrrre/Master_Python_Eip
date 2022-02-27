e5bc82bb0a1dc10f79c228103b54e6069f19109d
Víctor <victorluque341@gmail.com>
2022-02-28 00:46:37 +0100

    Actualizada fecha y descripción de Script

--- programacion_avanzada_python/06.paquetes_y_modulos/main.py
+++ programacion_avanzada_python/06.paquetes_y_modulos/main.py
@@ -1,3 +1,30 @@
+"""
+Trabajando con paquetes y modulos. Crea un paquete con la siguiente estructura:
+- main.py
+- paquete
+    - subpaquete_1
+        - mod1.py
+        - mod2.py
+    - subpaquete_2
+        - mod3.py
+        - mod4.py
+
+Añade una función print que muestre los nombres de los módulos del paquete.
+Importa el paquete y llama a los diferentes módulos.
+
+
+Script creado para la asignatura de Programación Avanzada en Python de la Escuela Internacional de Postgrados
+
+Uso: 
+- Windows: python "main.py"
+- Linux: python3 "main.py"
+
+@autor: Víctor Luque Martín
+@fecha: 28-02-2022
+@versión: 1.1
+@licencia: MIT
+@email: victorluque341@gmail.com
+"""
 from package.sub_package1.mod1 import module_name as mod1
 from package.sub_package1.mod2 import module_name as mod2
 from package.sub_package2.mod3 import module_name as mod3
