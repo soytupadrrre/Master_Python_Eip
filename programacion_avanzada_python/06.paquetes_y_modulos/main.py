c8ef6e6b39a2cfbb39d93d07ce2c8e1083e3d1e7
Víctor <victorluque341@gmail.com>
2022-02-28 00:42:45 +0100

    Paquetes y módulos en Python

---
+++ programacion_avanzada_python/06.paquetes_y_modulos/main.py
@@ -0,0 +1,10 @@
+from package.sub_package1.mod1 import module_name as mod1
+from package.sub_package1.mod2 import module_name as mod2
+from package.sub_package2.mod3 import module_name as mod3
+from package.sub_package2.mod4 import module_name as mod4
+
+if __name__ == "__main__":
+    mod1()
+    mod2()
+    mod3()
+    mod4()
---
+++ programacion_avanzada_python/06.paquetes_y_modulos/package/sub_package2/mod4.py
@@ -0,0 +1,5 @@
+def module_name():
+    print("Soy el módulo 4")
+
+if __name__ == "__main__":
+    print(module_name())
