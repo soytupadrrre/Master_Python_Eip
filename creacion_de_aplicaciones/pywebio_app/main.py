68914944420b2aa9e209f19559e7f88d1b05ac71
Víctor <victorluque341@gmail.com>
2022-05-24 00:52:33 +0200

    Added TODOs

--- creacion_de_aplicaciones/pywebio_app/main.py
+++ creacion_de_aplicaciones/pywebio_app/main.py
@@ -2,4 +2,16 @@ from pywebio.input import *
 from pywebio.output import *
 
 # Hello world
+# TODO: Ver datos de Iris Dataset
+
+# TODO: Mostrar matríz de confusión
+
+# TODO: Predecir datos
+
+# TODO: Insertar datos
+
+# TODO: Actualizar datos
+
+# TODO: Eliminar datos
+
 put_text("Hello world!")
