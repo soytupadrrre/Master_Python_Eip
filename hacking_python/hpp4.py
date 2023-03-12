3f807f6e2f9d89531f98366da9ab3965bb971e35
soytupadrrre <victorluque341@gmail.com>
2023-03-12 15:08:12 -0400

    Added Interface

--- hacking_python/hpp4.py
+++ hacking_python/hpp4.py
@@ -8,8 +8,10 @@ def main(args):
     :param args: Arguments
     :type args: argparse.Namespace
     """
+    src_mac = get_if_hwaddr(args.interface)
+    src_ip = get_if_addr(args.interface)
     ether_packet = Ether(dst=ETHER_BROADCAST)
-    arp_packet = ARP(pdst=args.target)
+    arp_packet = ARP(pdst=args.target, hwsrc=src_mac, psrc=src_ip)
     arp_request = ether_packet/arp_packet
     ans, _ = srp(arp_request, timeout=1, verbose=0)
     for _, rcv in ans:
@@ -18,5 +20,6 @@ def main(args):
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('-t', '--target', help='Target IP or IP range (192.168.1.54 or 192.168.0.0/24)', required=True)
+    parser.add_argument('-i', '--interface', help='Interface to use', default="eth0")
     args = parser.parse_args()
     main(args)
